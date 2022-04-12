cf69085a1d9f23c5ddc6d2cbd060d33ecc2da1ff
coding-At-GitHub <101490518+coding-At-GitHub@users.noreply.github.com>
2022-04-12 15:38:23 +0530

    Update bubbleSort.java

--- bubbleSort.java
+++ bubbleSort.java
@@ -13,6 +13,7 @@ class Main {
        int arr[] = {7, 8, 1, 3, 2};
  
        //bubble sort
+      //time complexity O(n^2)
        for(int i=0; i<arr.length-1; i++) {
            for(int j=0; j<arr.length-i-1; j++) {
                if(arr[j] > arr[j+1]) {
@@ -26,3 +27,4 @@ class Main {
  
        printArray(arr);
    }
+}
